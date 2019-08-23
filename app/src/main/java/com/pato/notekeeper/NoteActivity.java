3a768919583795408fa773bbf4e156fc0a00a65c
Patiwagura <patiwagura@gmail.com>
2019-08-24 01:22:26 +0300

    Changed passing a parcelable object to intent, instead position of selected note is passed as intent extra.

--- app/src/main/java/com/pato/notekeeper/NoteActivity.java
+++ app/src/main/java/com/pato/notekeeper/NoteActivity.java
@@ -2,11 +2,11 @@ package com.pato.notekeeper;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -15,9 +15,15 @@ import java.util.List;
 
 public class NoteActivity extends AppCompatActivity {
     //Qualify the constant with the package name to make it unique, to differentiate other constants used somewhere in code.
-    public static final String NOTE_INFO ="com.pato.notekeeper.NOTE_INFO";
+    public static final String NOTE_POSITION = "com.pato.notekeeper.NOTE_POSITION"; //constant pointing to the position of the selected Note.
+    public static final int POSITION_NOT_SET = -1;  //position was not set.
     private NoteInfo mSelectedNote;
     private boolean mIsNewNote; //checks if we are creating a new Note.
+    private EditText mTxtNoteTitle;
+    private EditText mTxtNoteText;
+    private Spinner mSpinnerCourses;
+    private int mNotePosition;
+    private boolean mIsCancelling;  //if true user want to exit without saving changes.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,23 +33,23 @@ public class NoteActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         //get reference to EditText
-        EditText txtNoteTitle = findViewById(R.id.text_note_title);
-        EditText txtNoteText = findViewById(R.id.text_note_text);
+        mTxtNoteTitle = findViewById(R.id.text_note_title);
+        mTxtNoteText = findViewById(R.id.text_note_text);
 
         //spinner reference.
-        Spinner spinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
+        mSpinnerCourses = (Spinner) findViewById(R.id.spinner_courses);
         List<CourseInfo> lstCourses = DataManager.getInstance().getCourses(); //get courses from data_manager
-        ArrayAdapter<CourseInfo> arrAdapterCourses = new ArrayAdapter<>(this,android.R.layout.simple_spinner_item,lstCourses);
+        ArrayAdapter<CourseInfo> arrAdapterCourses = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, lstCourses);
         arrAdapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //link spinner with adapter.
-        spinnerCourses.setAdapter(arrAdapterCourses);
+        mSpinnerCourses.setAdapter(arrAdapterCourses);
 
         //method to retrieve object_instance from intent extras and display.
         readDisplayStateValues();
 
         //method to display a note. Only displayNote when there is a note.
-        if(!mIsNewNote)
-        displayNote(spinnerCourses,txtNoteTitle, txtNoteText);
+        if (!mIsNewNote)
+            displayNote(mSpinnerCourses, mTxtNoteTitle, mTxtNoteText);
 
     }
 
@@ -60,10 +66,88 @@ public class NoteActivity extends AppCompatActivity {
 
     private void readDisplayStateValues() {
         Intent myIntent = getIntent(); //get intent that started this activity.
-        mSelectedNote = myIntent.getParcelableExtra(NOTE_INFO);
+        //mSelectedNote = myIntent.getParcelableExtra(NOTE_POSITION);  //recreate the note that was passed as an intent Extra.
+        int notePosition = myIntent.getIntExtra(NOTE_POSITION, POSITION_NOT_SET); //value-type Extras require a second_param as default value.
 
-        //if we don't pass an intent Extra mSelectedNote is null hence isNewNote = true, else isNewNote = false.
-        mIsNewNote = mSelectedNote == null;
+        //Logic: if we don't pass an intent Extra mSelectedNote is null hence isNewNote = true, else isNewNote = false.
+        //mIsNewNote = mSelectedNote == null;
+        mIsNewNote = notePosition == POSITION_NOT_SET;
+        //check if we are not creating new note, retrieve the note.
+        if (mIsNewNote) {
+            createNewNote(); //handle create new note.
+        } else {
+            mSelectedNote = DataManager.getInstance().getNotes().get(notePosition); //get note at position specified.
+        }
+
     }
 
+    //create a new note.
+    private void createNewNote() {
+        DataManager dm = DataManager.getInstance(); //get instance.
+        //create a new note and return its position.
+        mNotePosition = dm.createNewNote();
+        mSelectedNote = dm.getNotes().get(mNotePosition); //retrive not from List using index_notePosition.
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //inflate our menu
+        MenuInflater mnuInflater = getMenuInflater();
+        mnuInflater.inflate(R.menu.menu_note, menu); //inflate menu.
+        //return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // called when we select a menuItem.
+        int id = item.getItemId(); //get id of selected menuItem.
+
+        if (id == R.id.action_send_mail) {
+            //send email action.
+            sendEmail();
+            return true;
+        } else if (id == R.id.action_cancel) {
+            //user wants to cancel without saving the changes.
+            mIsCancelling = true;
+            finish(); //signal activity to exit.
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //when user exits Activity by hitting Back_button, we save changes made by user.
+        if (mIsCancelling) {
+            //user want to exit without saving changes.
+            if(mIsNewNote)
+                DataManager.getInstance().removeNote(mNotePosition);//remove newNote_position if user cancels in the process of creating a new note.
+        } else {
+            saveNote(); //save any changes.
+        }
+
+    }
+
+    private void saveNote() {
+        //save details of current selected note from our app.
+        mSelectedNote.setCourse((CourseInfo) mSpinnerCourses.getSelectedItem());  //currently selected course on our spinner.
+        mSelectedNote.setTitle(mTxtNoteTitle.getText().toString());
+        mSelectedNote.setText(mTxtNoteText.getText().toString());
+    }
+
+    private void sendEmail() {
+        //the email should have a title/subject & body /message.
+        CourseInfo eCourse = (CourseInfo) mSpinnerCourses.getSelectedItem(); //get selected course.
+        String emSubject = mTxtNoteTitle.getText().toString();  //get Title of the course.
+        String emBody = "Checkout what I learned in the pluralsight course \"" + eCourse.getTitle() + "\" \n" + mTxtNoteText.getText().toString();
+
+        Intent emailIntent = new Intent(Intent.ACTION_SEND); //create Intent, we provide action in Intent constructor.
+        emailIntent.setType("message/rfc2822"); //a standard internet mime-type for sending email.
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, emSubject); //subject of email. as intent Extra
+        emailIntent.putExtra(Intent.EXTRA_TEXT, emBody); //body of the email. as intent Extra
+        startActivity(emailIntent);   //startActivity with this implicit intent.
+    }
 }
--- app/src/main/java/com/pato/notekeeper/NoteListActivity.java
+++ app/src/main/java/com/pato/notekeeper/NoteListActivity.java
@@ -3,7 +3,6 @@ package com.pato.notekeeper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,6 +13,8 @@ import android.widget.ListView;
 import java.util.List;
 
 public class NoteListActivity extends AppCompatActivity {
+
+    private ArrayAdapter<NoteInfo> mAdapterNotes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +40,8 @@ public class NoteListActivity extends AppCompatActivity {
        final ListView listNotes = findViewById(R.id.list_notes); //listView reference.
         List<NoteInfo> notes = DataManager.getInstance().getNotes();  //get notes.
         //create an adapter.
-        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
-        listNotes.setAdapter(adapterNotes);
+        mAdapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+        listNotes.setAdapter(mAdapterNotes);
 
         //add onitemclick listener to allow users to make a selection.
         listNotes.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -48,8 +49,11 @@ public class NoteListActivity extends AppCompatActivity {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 //show the item selected from the ListView.
                 Intent mIntent = new Intent(NoteListActivity.this, NoteActivity.class);
-                NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position); //get the item selected from ListView.
-                mIntent.putExtra(NoteActivity.NOTE_INFO, note);  //passing NoteInfo reference as an intent extra
+                //NoteInfo note = (NoteInfo) listNotes.getItemAtPosition(position); //get the item selected from ListView.
+                //mIntent.putExtra(NoteActivity.NOTE_POSITION, note);  //passing NoteInfo reference as an intent extra
+
+                //Logic changed. Earlier we created intent, retrieved our note at selected_position, then passed the reference of Note_object as an intent Extra.
+                mIntent.putExtra(NoteActivity.NOTE_POSITION, position);  //pass position of selected item and let DataManager retrieve our Note.
                 startActivity(mIntent);
 
             }
@@ -57,4 +61,17 @@ public class NoteListActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //notify arrayAdapter that data set has changed.
+        //mAdapterNotes.notifyDataSetChanged();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //notify arrayAdapter that data set has changed.
+        mAdapterNotes.notifyDataSetChanged();
+    }
 }
